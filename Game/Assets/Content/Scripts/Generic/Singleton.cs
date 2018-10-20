6e77ade491d9e2f123cc958f2a8cdd8d6eda2e33
Rowen Kuiper <r0w3nkuiper@hotmail.com>
2018-10-20 22:55:00 +0200

    singleton, scene things

---
+++ Game/Assets/Content/Scripts/Generic/Singleton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance { get; set; }
+
+    public static T Instance()
+    {
+        _instance = FindObjectOfType<T>();
+        return _instance;
+    }
+}
